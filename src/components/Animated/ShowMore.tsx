2ea1a3391984d35870fe8f2ffd0617c4d6412a81
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2022-10-12 17:00:48 +0530

    Feat:Added Showmore-less component

---
+++ src/components/Animated/ShowMore.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react"
+import { View, StyleSheet, Text, Button } from "react-native"
+import Animated, {
+  EasingNode,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated"
+
+const ShowMore = () => {
+  const height = useSharedValue(100)
+  const [showMore, setShowMore] = useState(true)
+
+  const cardAnmatedStyle = useAnimatedStyle(() => {
+    return {
+      height: height.value,
+    }
+  })
+
+  const showMoreLessHandler = () => {
+    // height.value = showMore
+    //   ? withSpring(400) setShowMore(false)
+    //   : withSpring(100) setShowMore(true)
+    if (showMore) {
+      height.value = withSpring(200, EasingNode.back)
+      setShowMore(false)
+    } else {
+      height.value = withSpring(100)
+      setShowMore(true)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.card, cardAnmatedStyle]}>
+        <Text ellipsizeMode="head" style={styles.para}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+          delectus. Sunt quibusdam quam iure distinctio ratione odio aliquid
+          facere, deleniti quidem, quas assumenda esse voluptatem, voluptatibus
+          porro id corrupti odit?
+        </Text>
+        <Button
+          title={showMore ? " show more" : "show less"}
+          onPress={showMoreLessHandler}
+        />
+      </Animated.View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  para: {
+    // textAlign: "left",
+    // overflow: "hidden",
+    flex: 1,
+  },
+  card: {
+    padding: 5,
+    margin: 10,
+    backgroundColor: "white",
+    height: 100,
+  },
+})
+export default ShowMore
--- src/config/components.ts
+++ src/config/components.ts
@@ -1,8 +1,9 @@
 import InstagramLike from "../components/Animated/InstagramLike"
+import ShowMore from "../components/Animated/ShowMore"
 
 const components = [
   { id: 1, screenName: "instagram-like", screenComponent: InstagramLike },
-  { id: 2, screenName: "twitter-like", screenComponent: InstagramLike },
+  { id: 2, screenName: "show-more", screenComponent: ShowMore },
 ]
 
 export default components
